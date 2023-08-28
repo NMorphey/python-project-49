b5768b99e68005536523351f900a872b8edf3a1f
Ivlev Nikita <thel4stofme@yandex.ru>
2023-08-28 18:32:57 +0300

    Add prompt input to brain_games.py
    Commit contains:
    1. import of the prompt module
    2. usage of the module in order go get player's name
    3. successful build with the changes made

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
+import prompt
+
 
 def main():
-    print('Welcome to the Brain Games!') 
-
+    print('Welcome to the Brain Games!')
+    player_name = prompt.string("May I have your name? ")
 
 if __name__ == '__main__':
     main()
