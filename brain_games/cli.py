e7a53999b76ec79384aad5578620dfbda57bd55a
Ivlev Nikita <thel4stofme@yandex.ru>
2023-08-29 16:04:41 +0300

    Add docstring to welcome_user function (cli.py)
    Also added a blank line at the end of the file.

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -2,6 +2,7 @@ import prompt
 
 
 def welcome_user():
+    """"Shows welcome message and receives player's name, then returns it."""
     print('Welcome to the Brain Games!')
     player_name = prompt.string("May I have your name? ")
     print(f'Hello, {player_name}!')
