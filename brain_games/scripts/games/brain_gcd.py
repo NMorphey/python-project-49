319a9330137581898cab56012516cdea34b96423
Ivlev Nikita <thel4stofme@yandex.ru>
2023-08-30 23:58:09 +0300

    Add brain-gcd game
    Commit contains:
    1. new file brain_gcd.py with the algorithm of the game
    2. brain_gcd:main added as a script to pyproject.toml
    3. successful build with the changes made

---
+++ brain_games/scripts/games/brain_gcd.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+from random import randint
+
+from brain_games.game import play_a_game
+
+
+def main():
+    game_rules = 'Find the greatest common divisor of given numbers.'
+    NUMBER_OF_ROUNDS = 3
+    MIN_RANDOM_NUMBER = 1
+    MAX_RANDOM_NUMBER = 100
+    playsets = []
+    for i in range(NUMBER_OF_ROUNDS):
+        number_1 = randint(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER)
+        number_2 = randint(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER)
+        max_possible_divisor = min(number_1, number_2)
+        correct_answer = 1
+        for i in range(2, max_possible_divisor + 1):
+            if number_1 % i == 0 and number_2 % i == 0:
+                correct_answer = i
+        question = f'{number_1} {number_2}'
+        playset = (question, correct_answer)
+        playsets.append(playset)
+
+    play_a_game(game_rules, playsets)
+
+
+if __name__ == '__main__':
+    main()
