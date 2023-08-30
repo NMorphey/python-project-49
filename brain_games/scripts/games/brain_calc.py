3310a19b386d5c3638b8d300acddf50adf248c5b
Ivlev Nikita <thel4stofme@yandex.ru>
2023-08-30 23:22:09 +0300

    Add brain-calc game
    Commit contains:
    1. new file brain-calc.py with the algorithm of the game
    2. brain-calc:main added as a script to pyproject.toml

---
+++ brain_games/scripts/games/brain_calc.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+from random import randint, choice
+
+from brain_games.game import play_a_game
+
+
+def main():
+    game_rules = 'What is the result of the expression?'
+    NUMBER_OF_ROUNDS = 3
+    MIN_RANDOM_NUMBER = 1
+    MAX_RANDOM_NUMBER = 40
+    playsets = []
+    for i in range(NUMBER_OF_ROUNDS):
+        operand_1 = randint(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER)
+        operand_2 = randint(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER)
+        operator = choice(['+', '-', '*'])
+        match operator:
+            case '+':
+                correct_answer = operand_1 + operand_2
+            case '-':
+                correct_answer = operand_1 - operand_2
+            case '*':
+                correct_answer = operand_1 * operand_2
+        question = f'{operand_1} {operator} {operand_2}'
+        playset = (question, correct_answer)
+        playsets.append(playset)
+    play_a_game(game_rules, playsets)
+    
+        
+if __name__ == '__main__':
+    main()
