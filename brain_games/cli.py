80b5ada6168f4739fd47b3253fbd5cb98c7ab2c7
Ivlev Nikita <thel4stofme@yandex.ru>
2023-09-04 21:59:30 +0300

    Sumplify cli.py in order to follow project tasks

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -2,8 +2,6 @@ import prompt
 
 
 def welcome_user():
-    """"Shows welcome message and receives player's name, then returns it."""
     print('Welcome to the Brain Games!')
     player_name = prompt.string("May I have your name? ")
     print(f'Hello, {player_name}!')
-    return player_name
