f68ddf875b8a87b2a6963a32b9cc11331e3e3104
Ivlev Nikita <thel4stofme@yandex.ru>
2023-09-05 14:50:03 +0300

    Rename constants and a variable

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -7,18 +7,18 @@ MIN_FIRST_NUMBER = 1
 MAX_FIRST_NUMBER = 30
 MIN_PROGRESSION_LEN = 5
 MAX_PROGRESSION_LEN = 10
-MIN_STEP = 2
-MAX_STEP = 9
+MIN_COMMON_DIFFERENCE = 2
+MAX_COMMON_DIFFERENCE = 9
 DESCRIPTION = 'What number is missing in the progression?'
 
 
 def get_playset():
     first_number = randint(MIN_FIRST_NUMBER, MAX_FIRST_NUMBER)
     progression_len = randint(MIN_PROGRESSION_LEN, MAX_PROGRESSION_LEN)
-    step = randint(MIN_STEP, MAX_STEP)
+    common_difference = randint(MIN_COMMON_DIFFERENCE, MAX_COMMON_DIFFERENCE)
     progression = range(first_number,
-                        first_number + progression_len * step + 1,
-                        step)
+                        first_number + progression_len * common_difference + 1,
+                        common_difference)
     progression = list(map(str, progression))
     excluded_number_index = randint(0, progression_len - 1)
     excluded_number = progression[excluded_number_index]
