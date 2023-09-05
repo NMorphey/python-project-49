826a37c43a31be22039da54cf4089e54f5bf95cb
Ivlev Nikita <thel4stofme@yandex.ru>
2023-09-05 14:41:09 +0300

    Move gcd finding to the new-assigned function

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -8,15 +8,17 @@ MAX_RANDOM_NUMBER = 100
 DESCRIPTION = 'Find the greatest common divisor of given numbers.'
 
 
+def get_gcd(number_1, number_2):
+    max_possible_divisor = min(number_1, number_2)
+    for i in range(max_possible_divisor, 0, -1):
+        if number_1 % i == 0 and number_2 % i == 0:
+            return i
+
+
 def get_playset():
     number_1 = randint(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER)
     number_2 = randint(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER)
-    max_possible_divisor = min(number_1, number_2)
-    correct_answer = 1
-    for i in range(max_possible_divisor, 1, -1):
-        if number_1 % i == 0 and number_2 % i == 0:
-            correct_answer = i
-            break
+    correct_answer = get_gcd(number_1, number_2)
     question = f'{number_1} {number_2}'
     playset = (question, correct_answer)
 
